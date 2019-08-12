60bd621e548d82c5c6a7f1db55a158e2b4816937
Javier2010204 <2010204javiergomez@gmail.com>
2019-08-11 22:02:48 -0600

    js para imagenes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,3 +22,12 @@ $(document).ready(function() {
   /* Activating Best In Place */
   jQuery(".best_in_place").best_in_place();
 });
+
+document.querySelectorAll("[type='file']").forEach(function(control){
+  control.addEventListener('change', function(ev){
+    console.log(ev.target.file[0].name);
+    console.log(this.id)
+
+    document.querySelector("[for='"+this.id+"']").innerHTML = ev.target.files[0].name;
+  })
+})
